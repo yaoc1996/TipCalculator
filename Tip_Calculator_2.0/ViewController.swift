992ab8f55ff4ac60b73159a0578ff8bcdfd8769c
Yaocheng Wu <yaoc1996@gmail.com>
2016-12-30 14:05:02 -0500

    Initial Commit

---
+++ Tip_Calculator_2.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tip_Calculator_2.0
+//
+//  Created by Yaocheng Wu on 12/30/16.
+//  Copyright © 2016 Yao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
